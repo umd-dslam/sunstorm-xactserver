66a1269a3e8e8f9de495857965695e521baa8f6a
Minghui Liu <minghui.liu.95@gmail.com>
2021-12-02 20:45:18 -0500

    Send txn data via UDF

--- xactserver/src/pg_dispatcher.rs
+++ xactserver/src/pg_dispatcher.rs
@@ -27,15 +27,12 @@ impl PgDispatcher {
             let mut txn_rx = txn_rx;
             // Continuously listen for new tuple data from the postgres watcher
             while let Some(mut buf) = txn_rx.recv().await {
-                while let Some(tup) = get_tuple(&mut buf) {
-                    println!("# {:?}", tup);
-                }
-
                 let ip_port: Vec<&str> = self.addr.split(":").collect();
                 let conn_str = format!("host={} port={} user=postgres application_name=#remotexact", ip_port[0], ip_port[1]);
                 
                 println!("connecting to local pg, conn_str: {}", conn_str);
-                let (_client, conn) = connect(&conn_str, NoTls).await.unwrap();
+                let (client, conn) = connect(&conn_str, NoTls).await.unwrap();
+
                 // The connection object performs the actual communication with the database,
                 // so spawn it off to run on its own.
                 tokio::spawn(async move {
@@ -44,22 +41,20 @@ impl PgDispatcher {
                     }
                 });
 
-                println!("connected to pg");
+                println!("connected to pg, sending transaction data");
 
+                // Copy buf to a new vec<u8> because tokio_postgres does not 
+                // know how to convert bytes::Bytes to postgres type
+                let mut txn_data: Vec<u8> = Vec::new();
+                while buf.has_remaining() {
+                    txn_data.push(buf.get_u8());
+                }
+
+                // Create extension remotexact if not already created
+                client.execute("CREATE EXTENSION IF NOT EXISTS remotexact", &[]).await.unwrap();
+                client.query("SELECT print_bytes($1::bytea);", &[&txn_data]).await.unwrap();
             }
         });
         Ok(())
     }
-}
-
-fn get_tuple(buf: &mut Bytes) -> Option<(i32, i32, i32, i16)> {
-    if buf.remaining() == 0 {
-        return None;
-    }
-    let dbid = buf.get_i32();
-    let rid = buf.get_i32();
-    let blockno = buf.get_i32();
-    let offset = buf.get_i16();
-
-    Some((dbid, rid, blockno, offset))
 }
