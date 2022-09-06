a9c09f93f66bb11de15a66874425217e625d4c90
Cuong Nguyen <ctring23@gmail.com>
2022-09-06 11:53:46 -0400

    Include region info in data items of rwset

--- xactserver/src/xact.rs
+++ xactserver/src/xact.rs
@@ -205,11 +205,13 @@ impl RWSetHeader {
 enum Relation {
     Table {
         oid: u32,
+        region: u8,
         csn: u32,
         tuples: Vec<Tuple>,
     },
     Index {
         oid: u32,
+        region: u8,
         pages: Vec<Page>,
     },
 }
@@ -239,6 +241,7 @@ impl Relation {
 
     fn decode_table(buf: &mut Bytes) -> anyhow::Result<Relation> {
         let relid = get_u32(buf).context("Failed to decode 'relid'")?;
+        let region = get_u8(buf).context("Failed to decode 'region'")?;
         let ntuples = get_u32(buf).context("Failed to decode 'ntuples'")?;
         let csn = get_u32(buf).context("Failed to decode 'csn'")?;
         let mut tuples = vec![];
@@ -250,6 +253,7 @@ impl Relation {
 
         Ok(Relation::Table {
             oid: relid,
+            region,
             csn,
             tuples,
         })
@@ -264,6 +268,7 @@ impl Relation {
 
     fn decode_index(buf: &mut Bytes) -> anyhow::Result<Relation> {
         let relid = get_u32(buf).context("Failed to decode 'relid'")?;
+        let region = get_u8(buf).context("Failed to decode 'region")?;
         let npages = get_u32(buf).context("Failed to decode 'npages'")?;
         let mut pages = vec![];
         for _ in 0..npages {
@@ -272,7 +277,7 @@ impl Relation {
             })?);
         }
 
-        Ok(Relation::Index { oid: relid, pages })
+        Ok(Relation::Index { oid: relid, region, pages })
     }
 
     fn decode_page(buf: &mut Bytes) -> anyhow::Result<Page> {
